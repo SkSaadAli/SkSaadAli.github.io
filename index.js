5bcbb92177365d51c92043d4fb37724092e25224
Saad <sk.saad3621@gmai.com>
2022-12-16 19:15:36 +0530

    Share button added

---
+++ index.js
@@ -0,0 +1,17 @@
+const shareButton = document.querySelector('.share-button');
+const shareDialog = document.querySelector('.share-dialog');
+const closeButton = document.querySelector('.close-button');
+
+shareButton.addEventListener('click', event => {
+  if (navigator.share) { 
+   navigator.share({
+      title: 'WebShare API Demo',
+      url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+    .catch(console.error);
+    } else {
+        return;
+    }
+});
