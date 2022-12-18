e0f3f7ef9c8c4d8d4b7a924573153ef48cbee799
Shaikh Saad Ali <109274606+SkSaadAli@users.noreply.github.com>
2022-12-18 13:05:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const shareButton = document.querySelector('.share-button');
-const shareDialog = document.querySelector('.share-dialog');
-const closeButton = document.querySelector('.close-button');
 
 shareButton.addEventListener('click', event => {
   if (navigator.share) { 
